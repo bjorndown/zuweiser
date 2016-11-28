643b61ecc5a867f1b2549967542ea19bf4f739b1
Björn Mosler <dev@bjoern.mosler.ch>
2016-11-28 21:49:10 +0100

    Clean up reader module.

--- src/reader.js
+++ src/reader.js
@@ -1,26 +1,45 @@
 const Excel = require('exceljs')
 const _ = require('lodash')
+const {log} = require('./log')
 
 const workbook = new Excel.Workbook()
 
+let skipFirstValueInRowMode = false
+
 function getRowsAsObjects(workbook, worksheetName) {
     const worksheet = workbook.getWorksheet(worksheetName)
-    const header = worksheet.getRow(1).values.splice(1)
+    const headerColumns = readHeaderColumns(worksheet)
+    writeHeaderColumnsToWorksheet(worksheet, headerColumns)
+    return readRows(worksheet, headerColumns)
+}
 
-    worksheet.columns = header.map(header =>  { 
+function readHeaderColumns(worksheet) {
+    const firstRow = worksheet.getRow(1).values
+
+    if (_.isEmpty(firstRow[0])) {
+        log.warn('Ignoring first value in every row since it was empty in header row.')
+        skipFirstValueInRowMode = true
+    }
+    
+    return skipFirstValueInRowMode ? firstRow.splice(1) : firstRow 
+}
+
+function writeHeaderColumnsToWorksheet(worksheet, headerColumns) {
+    worksheet.columns = headerColumns.map(header =>  {
         return { header: header, key: header }
     })
+}
 
-    const objects = []
-
-    workbook.getWorksheet(worksheetName).eachRow((row, rowNumber) => {
+function readRows(worksheet, headerColumns) {
+    const rowObjects = []
+    worksheet.eachRow((row, rowNumber) => {
         if (rowNumber > 1) {
-            const rowObj = _.zipObject(header, row.values.splice(1))
-            objects.push(rowObj)
+            const values = skipFirstValueInRowMode ? row.values.splice(1) : row.skipFirstValueMode
+            const rowObj = _.zipObject(headerColumns, values)
+            rowObjects.push(rowObj)
         }
     })
-
-    return objects
+    return rowObjects
 }
 
 function readExcel(filename) {
