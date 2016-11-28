adbbda4324a4e7ec706f9de9b15e54d33d52d506
Björn Mosler <dev@bjoern.mosler.ch>
2016-11-28 19:19:02 +0100

    Model courses and students as ES6 classes.

--- model.js
+++ model.js
@@ -1,44 +1,74 @@
-const _ = require('lodash');
+const _ = require('lodash')
 
 function mapRawToObjFields(rawObj, fields) {
-    let obj = {};
+    let obj = {}
     // TODO clean up
     _.forOwn(fields, (value, key) => {
         if (_.isString(value)) {
-            obj[key] = rawObj[value];
+            obj[key] = rawObj[value]
         } else if (_.isArray(value)) {
-            obj[key] = [];
+            obj[key] = []
             _.forOwn(value, (value2, key2) => {
-                obj[key].push(rawObj[value2.column]);
-            });
+                obj[key].push(rawObj[value2.column])
+            })
         }
-    });
-    return obj;
+    })
+    return obj
 }
 
 function convertStudent(rawStudent, studentFields) {
-    let student = mapRawToObjFields(rawStudent, studentFields);
-    // TODO move to class?
-    student.toString = function() { return this.firstName + ' ' + this.name };
-    student.unmatchedPriorities = function*() {
-        let index = 0;
-        while (this.matched === false && index < this.priorities.length) {
-            console.log('Matching priority ' + index + ' for ' + this)
-            yield this.priorities[index];
-            index = index + 1;
-        } 
-    };
-    student.matched = false;
-    return student;
+    let studentData = mapRawToObjFields(rawStudent, studentFields)
+    return new Student(studentData)
 }
 
 function convertCourse(rawCourse, courseFields) {
-    let course = mapRawToObjFields(rawCourse, courseFields);
-    // TODO move to class?
-    course.students = [];
-    course.waitingList = [];
-    course.toString = function() { return this.name };
-    return course;
+    let courseData = mapRawToObjFields(rawCourse, courseFields)
+    return new Course(courseData)
+}
+
+class Course {
+    constructor(courseData) {
+        this.courseData = courseData
+        this.students = []
+        this.waitingList = []
+    }
+
+    get id() {
+        return this.courseData.id
+    }
+
+    get limit() {
+        return this.courseData.limit
+    }
+
+    get name() {
+        return this.courseData.name
+    }
+
+    toString() {
+        return this.name;
+    }
+}
+
+class Student {
+
+    constructor(studentData) {
+        this.studentData = studentData;
+        this.matched = false;
+    }
+
+    *unmatchedPriorities() {
+        let index = 0;
+        while (this.matched === false && index < this.studentData.priorities.length) {
+            console.log('Matching priority ' + index + ' for ' + this)
+            yield this.studentData.priorities[index]
+            index = index + 1
+        }
+    }
+
+    toString() {
+        return this.studentData.firstName + ' ' + this.studentData.name
+    }
 }
 
 exports.convertStudent = convertStudent;
