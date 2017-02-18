b813db27bf28feef4b95d7c8d421397dca8cea95
Björn Mosler <dev@bjoern.mosler.ch>
2017-02-18 22:00:12 +1100

    Add toggle for displaying waiting lists.

--- src/gui/components/result-printer.vue
+++ src/gui/components/result-printer.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h2 v-if="result.courses">Resultat</h2>
+    <div v-if="result.courses">
+        <h2>Resultat</h2>
+
+        <label for="printWaitingList">Wartelisten anzeigen</label>
+        <input type="checkbox" v-model="printWaitingList">
+
         <ul>
             <li v-for="course in result.courses">
                 <h3>{{course.name}}</h3>
@@ -13,24 +17,31 @@
                         </tr>
                     </tbody>
                 </table>
-                <h4>Warteliste</h4>
-                <table>
-                    <tbody>
-                        <tr v-for="student in course.waitingList">
-                            <td>{{student.firstName}}</td>
-                            <td>{{student.name}}</td>
-                            <td>{{student.class}}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <template v-if="printWaitingList">
+                    <h4>Warteliste</h4>
+                    <table>
+                        <tbody>
+                            <tr v-for="student in course.waitingList">
+                                <td>{{student.firstName}}</td>
+                                <td>{{student.name}}</td>
+                                <td>{{student.class}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </template>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-export default {
-    props: ['result']
-}
+    export default {
+        props: ['result'],
+        data() {
+            return {
+                printWaitingList: false
+            }
+        }
+    }
 
 </script>
