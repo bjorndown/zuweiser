701f483d3ef76cb20caa015eaeb7dce2aca8fb9d
Björn Mosler <dev@bjoern.mosler.ch>
2017-02-21 20:31:34 +1100

    Fix uglify.
    Add .babelrc, otherwise .vue singe file components are not compiled and then Uglify fails with 'Unexpected token punc [..]'

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /(node_modules)/,
-      loader: 'babel-loader',
-      query: {
-        presets: [['es2015']],
-      }
+      loader: 'babel-loader'
     },
     {
       test: /\.vue$/,
@@ -28,7 +25,7 @@ module.exports = {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: "url-loader?limit=10000&minetype=application/font-woff"
     },
-    {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
+    { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   resolve: {
