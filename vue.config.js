23f182df3396b5a57cb5d3d0015aafa3bcdb6170
Björn <dev@bjoern.mosler.ch>
2020-01-29 22:02:48 +0100

    Add deploy action. (#3)
    * Add deploy action.
    
    * Fix targets.
    
    * Add deployment step.
    
    * Fix public path.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/zuweiser/' : '/'
+
 module.exports = {
+    publicPath,
     configureWebpack: {
         entry: './src/gui/main.ts'
     }
