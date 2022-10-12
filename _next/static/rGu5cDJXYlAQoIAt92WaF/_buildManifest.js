563e28591261c4b63e12d3cb34e83c2a27f73450
bjorm <bjorm@users.noreply.github.com>
2022-10-12 13:31:39 +0000

    deploy: 12a7d3448eb160f66adfa9229ec3ba962295ef27

---
+++ _next/static/rGu5cDJXYlAQoIAt92WaF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/167-79fe73b420023e1d.js","static/chunks/pages/index-498e7b8f784c2156.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
