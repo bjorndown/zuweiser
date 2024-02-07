8440aefcdceb493ddee54c037979f47f0cc990ff
bjorndown <bjorndown@users.noreply.github.com>
2024-02-07 21:35:46 +0000

    deploy: 955098b6a0bcca6faa09adba11d7748e76a4ebe1

--- _next/static/mg4IWx953q1whv1QRLXM5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/175-a9cbb4398db24b9b.js","static/css/431084fa6ece677f.css","static/chunks/pages/index-683b3b8231ce6874.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
